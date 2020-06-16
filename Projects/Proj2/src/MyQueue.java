f97ed47295699fd67cfc7be81691005eb8dbb215
justin-henley <justin.a.henley@gmail.com>
2020-06-16 15:26:18 +0800

    Project 2 started; source files imported

---
+++ Projects/Proj2/src/MyQueue.java
@@ -0,0 +1,63 @@
+/**
+ * class MyQueue implemented using ArrayList. The index 0 element is the front of the queue
+ * The last element of the queue has index tail
+ * @author Your Name
+ * @version Date
+ */
+import java.util.ArrayList;
+
+public class MyQueue<E>
+{
+    private ArrayList<E> list; // hold the elements in queue
+    private int tail; // index of the last element in queue
+    
+    /**
+     * constructor construct an empty queue
+     */
+    public MyQueue()
+    {
+    }
+    
+    /**
+     * isEmpty return true if the queue is empty; false otherwise
+     * @return true if the queue is empty; false otherwise
+     */
+    public boolean isEmpty()
+    {
+       
+    }
+    
+    /**
+     * size return the size of the queue
+     * @return the number of elements in queue
+     */
+    public int size()
+    {
+        
+    }
+    
+    /**
+     * peek return the front element of the queue
+     * @return the front element of the queue. If the queue is empty, return null
+     */
+    public E peek()
+    {
+        
+    }
+    
+    /**
+     * pop remove the front element of the queue
+     */
+    public void pop()
+    {
+       
+    }
+    
+    /**
+     * push push a new element to the queue
+     */
+    public void push(E item)
+    {
+        
+    }
+}
---
+++ Projects/Proj2/src/MyStack.java
@@ -0,0 +1,65 @@
+/**
+ * class MyStack: A stack class implemented by using ArrayList
+ * All stack elements are stored in an ArrayList. The top element
+ * has index top
+ * 
+ * @author Your Name
+ * @version Date
+ */
+import java.util.ArrayList;
+
+public class MyStack<E>
+{
+    private ArrayList<E> list; // used to store elements in stack
+    private int top; // the index of top element
+    
+    /**
+     * constructor construct an empty stack
+     */
+    public MyStack()
+    {
+        
+    }
+    
+    /**
+     * push push a given element on the top of the stack
+     */
+    public void push(E item)
+    {
+        
+    }
+    
+    /**
+     * isEmpty return true if the stack is empty; false otherwise
+     * @return true if the stack is empty; false otherwise
+     */
+    public boolean isEmpty()
+    {
+        
+    }
+    
+    /**
+     * peek Return the top element
+     */
+    public E peek()
+    {
+       
+    }
+    
+    /**
+     * pop Remove the top element from the stack. If the stack is empty,nothing happen
+     */
+    public void pop()
+    {
+       
+    }
+    
+    /**
+     * size return the size of the stack
+     * @return number of elements in stack
+     */
+    public int size()
+    {
+
+    }
+}
